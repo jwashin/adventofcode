2c58b8e8734b28f140b38b03f33565676dd74f20
Jim Washington <washington.jim@gmail.com>
2022-09-16 06:55:54 -0400

    day19 2015 done

--- 2015/day19/day19_test.go
+++ 2015/day19/day19_test.go
@@ -51,3 +51,72 @@ func Test_moleculeCount(t *testing.T) {
 		})
 	}
 }
+
+func Test_fewestSteps(t *testing.T) {
+	type args struct {
+		target string
+		rules  []rule
+		start  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{"HOH", []rule{{"H", "HO"}, {"H", "OH"}, {"O", "HH"}, {"e", "H"}, {"e", "O"}}, "e"}, 3},
+		{"2", args{"HOHOHO", []rule{{"H", "HO"}, {"H", "OH"}, {"O", "HH"}, {"e", "H"}, {"e", "O"}}, "e"}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fewestSteps(tt.args.target, tt.args.rules, tt.args.start); got != tt.want {
+				t.Errorf("fewestSteps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_singleUnReplacement(t *testing.T) {
+	type args struct {
+		s          string
+		toString   string
+		fromString string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"1", args{"HOOH", "HO", "H"}, []string{"HOH"}},
+		{"2", args{"HOHO", "HO", "H"}, []string{"HOH", "HHO"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleUnReplacement(tt.args.s, tt.args.toString, tt.args.fromString); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("singleUnReplacement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_fewestStepsA(t *testing.T) {
+	type args struct {
+		target string
+		rules  []rule
+		start  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{"e", []rule{{"H", "HO"}, {"H", "OH"}, {"O", "HH"}, {"e", "H"}, {"e", "O"}}, "HOH"}, 3},
+		{"2", args{"e", []rule{{"H", "HO"}, {"H", "OH"}, {"O", "HH"}, {"e", "H"}, {"e", "O"}}, "HOHOHO"}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fewestStepsA(tt.args.target, tt.args.rules, tt.args.start); got != tt.want {
+				t.Errorf("fewestStepsA() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

91618ba614141c024bc32eeaae84ec04b5c9a272
Jim Washington <washington.jim@gmail.com>
2022-12-09 11:57:01 -0500

    gotta fix the negativity

--- 2022/day9/day9.go
+++ 2022/day9/day9.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	input, _ := os.ReadFile("input.txt")
-	fmt.Println("part 1:", follow(string(input)))
+	fmt.Println("part 1:", follow(string(input), 2))
+	fmt.Println("part 2:", follow(string(input), 10))
 }
 
 type coordinate struct {
@@ -17,15 +18,36 @@ type coordinate struct {
 	y int
 }
 
-func follow10(s string, count int) int {
+func follow(s string, count int) int {
 	data := strings.Split(s, "\n")
-	nTails := 10
+	nTails := count
 	rope := []*coordinate{}
 	for x := 0; x < nTails; x++ {
 		rope = append(rope, &coordinate{0, 0})
 	}
 
 	tailPositions := map[string]int{"0 0": 1}
+	d := map[coordinate]coordinate{}
+	// dx, dy: movex, movey
+	d[coordinate{0, -2}] = coordinate{0, 1}
+	d[coordinate{0, 2}] = coordinate{0, -1}
+	d[coordinate{-2, 0}] = coordinate{1, 0}
+	d[coordinate{2, 0}] = coordinate{-1, 0}
+
+	d[coordinate{2, 1}] = coordinate{-1, -1}
+	d[coordinate{2, -1}] = coordinate{-1, 1}
+	d[coordinate{-2, 1}] = coordinate{1, -1}
+	d[coordinate{-2, -1}] = coordinate{1, 1}
+
+	d[coordinate{1, 2}] = coordinate{-1, -1}
+	d[coordinate{1, -2}] = coordinate{-1, 1}
+	d[coordinate{-1, 2}] = coordinate{1, -1}
+	d[coordinate{-1, -2}] = coordinate{1, 1}
+
+	d[coordinate{-2, -2}] = coordinate{1, 1}
+	d[coordinate{2, 2}] = coordinate{-1, -1}
+	d[coordinate{-2, 2}] = coordinate{1, -1}
+	d[coordinate{2, -2}] = coordinate{-1, 1}
 
 	for _, v := range data {
 		f := strings.Fields(v)
@@ -51,166 +73,23 @@ func follow10(s string, count int) int {
 			case 'R':
 				head.x += 1
 			}
-			// tail move
-			for k, v := range rope[1:] {
-				currX := rope[k-1].x
-				currY := rope[k-1].y
-				tailX := v.x
-				tailY := v.y
-				isLast := v == rope[len(rope)-1]
-				if currX == tailX && currY-tailY == -2 {
-					v.y -= 1
+			// tail moves
+			for k, v := range rope {
+				if k > 0 {
+					currX := rope[k-1].x
+					currY := rope[k-1].y
+					tailX := v.x
+					tailY := v.y
+					isLast := k == len(rope)-1
+
+					move := d[coordinate{currX - tailX, currY - tailY}]
+					v.x -= move.x
+					v.y -= move.y
 					if isLast {
 						tailPositions[fmt.Sprintf("%d %d", v.x, v.y)] += 1
 					}
-					continue
-				}
-				if currX == tailX && currY-tailY == 2 {
-					v.y += 1
-					if isLast {
-						tailPositions[fmt.Sprintf("%d %d", v.x, v.y)] += 1
-					}
-					continue
-				}
-
-				if currY == tailY && currX-tailX == -2 {
-					v.x -= 1
-					if isLast {
-						tailPositions[fmt.Sprintf("%d %d", v.x, v.y)] += 1
-					}
-					continue
-				}
-				if currY == tailY && currX-tailX == 2 {
-					v.x += 1
-					if isLast {
-						tailPositions[fmt.Sprintf("%d %d", v.x, v.y)] += 1
-					}
-					continue
-				}
-
-				if currX-tailX == 2 && (currY-tailY == 1 || currY-tailY == -1) {
-					v.x += 1
-					v.y = currY
-					if isLast {
-						tailPositions[fmt.Sprintf("%d %d", v.x, v.y)] += 1
-					}
-					continue
-				}
-
-				if currX-tailX == -2 && (currY-tailY == 1 || currY-tailY == -1) {
-					v.x -= 1
-					v.y = currY
-					if isLast {
-						tailPositions[fmt.Sprintf("%d %d", v.x, v.y)] += 1
-					}
-					continue
-				}
-
-				if currY-tailY == -2 && (currX-tailX == 1 || currX-tailX == -1) {
-					v.y -= 1
-					v.x = currX
-					if isLast {
-						tailPositions[fmt.Sprintf("%d %d", v.x, v.y)] += 1
-					}
-					continue
-				}
-
-				if currY-tailY == 2 && (currX-tailX == 1 || currX-tailX == -1) {
-					v.y += 1
-					v.x = currX
-					if isLast {
-						tailPositions[fmt.Sprintf("%d %d", v.x, v.y)] += 1
-					}
-					continue
 				}
 			}
-		}
-	}
-	return len(tailPositions)
-}
-
-func follow(s string) int {
-	data := strings.Split(s, "\n")
-
-	currX, currY := 0, 0
-	tailX, tailY := 0, 0
-
-	tailPositions := map[string]int{"0 0": 1}
-
-	for _, v := range data {
-		f := strings.Fields(v)
-		direction := f[0]
-		dist, _ := strconv.Atoi(f[1])
-		moveQ := ""
-		for m := 0; m < dist; m++ {
-			moveQ += direction
-		}
-		for len(moveQ) > 0 {
-
-			move := moveQ[0]
-			moveQ = moveQ[1:]
-			// head move
-			switch move {
-			case 'U':
-				currY += 1
-			case 'D':
-				currY -= 1
-			case 'L':
-				currX -= 1
-			case 'R':
-				currX += 1
-			}
-			// tail move
-			if currX == tailX && currY-tailY == -2 {
-				tailY -= 1
-				tailPositions[fmt.Sprintf("%d %d", tailX, tailY)] += 1
-				continue
-			}
-			if currX == tailX && currY-tailY == 2 {
-				tailY += 1
-				tailPositions[fmt.Sprintf("%d %d", tailX, tailY)] += 1
-				continue
-			}
-
-			if currY == tailY && currX-tailX == -2 {
-				tailX -= 1
-				tailPositions[fmt.Sprintf("%d %d", tailX, tailY)] += 1
-				continue
-			}
-			if currY == tailY && currX-tailX == 2 {
-				tailX += 1
-				tailPositions[fmt.Sprintf("%d %d", tailX, tailY)] += 1
-				continue
-			}
-
-			if currX-tailX == 2 && (currY-tailY == 1 || currY-tailY == -1) {
-				tailX += 1
-				tailY = currY
-				tailPositions[fmt.Sprintf("%d %d", tailX, tailY)] += 1
-				continue
-			}
-
-			if currX-tailX == -2 && (currY-tailY == 1 || currY-tailY == -1) {
-				tailX -= 1
-				tailY = currY
-				tailPositions[fmt.Sprintf("%d %d", tailX, tailY)] += 1
-				continue
-			}
-
-			if currY-tailY == -2 && (currX-tailX == 1 || currX-tailX == -1) {
-				tailY -= 1
-				tailX = currX
-				tailPositions[fmt.Sprintf("%d %d", tailX, tailY)] += 1
-				continue
-			}
-
-			if currY-tailY == 2 && (currX-tailX == 1 || currX-tailX == -1) {
-				tailY += 1
-				tailX = currX
-				tailPositions[fmt.Sprintf("%d %d", tailX, tailY)] += 1
-				continue
-			}
-
 		}
 	}
 	return len(tailPositions)
--- 2022/day9/day9_test.go
+++ 2022/day9/day9_test.go
@@ -2,9 +2,19 @@ package main
 
 import "testing"
 
+// {"1", args{`R 4
+// U 4
+// L 3
+// D 1
+// R 4
+// D 1
+// L 5
+// R 2`}, 13},
+
 func Test_follow(t *testing.T) {
 	type args struct {
-		s string
+		s     string
+		count int
 	}
 	tests := []struct {
 		name string
@@ -12,17 +22,33 @@ func Test_follow(t *testing.T) {
 		want int
 	}{
 		{"1", args{`R 4
-		U 4
-		L 3
-		D 1
-		R 4
-		D 1
-		L 5
-		R 2`}, 13},
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2`, 2}, 13},
+		{"2", args{`R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2`, 10}, 1},
+		{"3", args{`R 5
+		U 8
+		L 8
+		D 3
+		R 17
+		D 10
+		L 25
+		U 20`, 10}, 36},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := follow(tt.args.s); got != tt.want {
+			if got := follow(tt.args.s, tt.args.count); got != tt.want {
 				t.Errorf("follow() = %v, want %v", got, tt.want)
 			}
 		})

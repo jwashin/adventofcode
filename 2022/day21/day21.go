aa30ca4922f211e05b7db67446c6d39f202fb9de
Jim Washington <washington.jim@gmail.com>
2022-12-22 05:05:30 -0500

    moving on to day 22

--- 2022/day21/day21.go
+++ 2022/day21/day21.go
@@ -9,166 +9,176 @@ import (
 
 func main() {
 	input, _ := os.ReadFile("input.txt")
-	fmt.Println(part1(string(input)))
-	fmt.Println(part2(string(input)))
+	fmt.Println("part 1:", part1(string(input)))
+	fmt.Println("part 2:", part2(string(input)))
 }
+
+// 134 too low
+// 4418 too low
+
+var revop = map[string]string{"+": "-", "-": "+", "*": "/", "/": "*"}
 
 func part2(s string) int {
 	data := strings.Split(s, "\n")
-	humn := -1
-	for {
-		values := map[string]int{}
-		fns := map[string][3]string{}
-		for _, v := range data {
-			j := strings.Fields(v)
-			key := j[0][:4]
-			if len(j) == 2 {
-				value, _ := strconv.Atoi(j[1])
-				values[key] = value
-			}
-			if len(j) == 4 {
-				t := [3]string{}
-				t[0] = j[1]
-				t[1] = j[2]
-				t[2] = j[3]
-				fns[key] = t
-			}
-		}
-		humn += 1
-		values["humn"] = humn
-		q := []string{}
-		currItem := ""
-		for ky := range fns {
-			q = append(q, ky)
-		}
-		for len(q) > 0 {
-			currItem = q[0]
-			q = q[1:]
-
-			v1, v2 := 0, 0
-			ok1, ok2 := false, false
-
-			v1s := fns[currItem][0]
-			v2s := fns[currItem][2]
-			op := fns[currItem][1]
-
-			v1, ok1 = values[v1s]
-			v2, ok2 = values[v2s]
-
-			if ok1 && ok2 {
-				// if currItem == "vslw"{
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "hpnl" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "whwg" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "fhhm" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "zlmt" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "fgvl" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "hstf" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "stzw" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "mzsz" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "vbbc" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "nhvm" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "bfcg" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "vbdq" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "ttgc" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "tztg" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "bbgj" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "gzdf" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "rlwp" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "drdn" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "bsbv" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "fgrn" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "mlvd" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "wtzd" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "ntnz" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "frpz" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "pjbq" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "dzbb" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "frlr" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "glgl" {
-				// 	fmt.Println(v1, v2)
-				// }
-				// if currItem == "sbtm" {
-				// 	fmt.Println(v1, v2)
-				// }
-				if currItem == "root" {
-					if v1 == v2 {
-						return values["humn"]
-					}
-				}
-				// perform operation
-				value := 0
-				if op == "*" {
-					value = v1 * v2
-				} else if op == "+" {
-					value = v1 + v2
-				} else if op == "/" {
-					value = v1 / v2
-				} else if op == "-" {
-					value = v1 - v2
-				}
-				// add value to values
-				values[currItem] = value
-			} else {
-				q = append(q, currItem)
-			}
+	values := map[string]int{}
+	fns := map[string][3]string{}
+	for _, v := range data {
+		j := strings.Fields(v)
+		key := j[0][:4]
+		if len(j) == 2 {
+			value, _ := strconv.Atoi(j[1])
+			values[key] = value
 		}
 	}
+	for _, v := range data {
+		j := strings.Fields(v)
+		key := j[0][:4]
+		if len(j) == 4 {
+			a := j[1]
+			op := j[2]
+			b := j[3]
+			t := [3]string{}
+			t[0] = a
+			t[1] = op
+			t[2] = b
+			fns[key] = t
+
+			// t = [3]string{}
+			// t[0] = key
+			// t[1] = revop[op]
+			// t[2] = b
+			// fns[a] = t
+
+			// if strings.Contains("*+", op) {
+			// 	t = [3]string{}
+			// 	t[0] = key
+			// 	t[1] = revop[op]
+			// 	t[2] = a
+			// 	fns[b] = t
+			// }
+
+			// if strings.Contains("-/", op) {
+			// 	t = [3]string{}
+			// 	t[0] = a
+			// 	t[1] = op
+			// 	t[2] = key
+			// 	fns[b] = t
+			// }
+		}
+	}
+	delete(values, "humn")
+	// the second one is for test
+	// fns["humn"] = [3]string{"zdwl", "+", "bnrn"}
+	fns["humn"] = [3]string{"dvpt", "+", "ptdq"}
+
+	q := []string{}
+	currItem := ""
+	for ky := range fns {
+		if _, ok := values[ky]; !ok {
+			q = append(q, ky)
+		}
+	}
+	for len(q) > 0 {
+		currItem = q[0]
+		q = q[1:]
+
+		_, ok0 := values[currItem]
+
+		v1, v2 := 0, 0
+		ok1, ok2 := false, false
+
+		v1s := fns[currItem][0]
+		v2s := fns[currItem][2]
+		op := fns[currItem][1]
+
+		v1, ok1 = values[v1s]
+		v2, ok2 = values[v2s]
+
+		if ok1 && ok2 {
+			// we have both. perform operation
+			value := 0
+			if op == "*" {
+				value = v1 * v2
+			} else if op == "+" {
+				value = v1 + v2
+			} else if op == "/" {
+				value = v1 / v2
+			} else if op == "-" {
+				value = v1 - v2
+			}
+			if currItem == "humn" {
+				return value
+			}
+			values[currItem] = value
+
+		} else if (ok1 || ok2) && currItem == "root" {
+			if ok1 {
+				values[v2s] = v1
+				// q = append(q, v1s)
+				// q = append(q, v2s)
+			}
+			if ok2 {
+				values[v1s] = v2
+				// q = append(q, v2s)
+				// q = append(q, v1s)
+			}
+		}
+		if ok0 && ok1 {
+			fmt.Println("ok1")
+
+			//  in c = a $ b
+			// we have c and a, need b
+			t := [3]string{}
+
+			a := v1s
+			key := v2s
+			b := currItem
+
+			if strings.Contains("*+", op) {
+				t[0] = key
+				t[1] = revop[op]
+				t[2] = a
+				fns[b] = t
+			}
+
+			if strings.Contains("-/", op) {
+				t[0] = a
+				t[1] = op
+				t[2] = key
+				fns[b] = t
+			}
+			// q = append(q, currItem)
+
+		}
+		if ok0 && ok2 {
+			fmt.Println("ok2")
+			t := [3]string{}
+			t[0] = currItem
+			t[1] = revop[op]
+			t[2] = v2s
+			fns[v1s] = t
+			// q = append(q, v1s)
+			// q = append(q, currItem)
+		} else {
+			q = append(q, currItem)
+		}
+
+		// if ok1 || ok2 {
+		// 	if ok1 {
+		// 	}
+		// 	if ok2 {
+		// 	}
+		// 	// perform the reverse operation, and we're done
+		// }
+
+	}
+	// remaining := q[0]
+	// r := values[remaining]
+	// st := values[fns[remaining][2]]
+	// fmt.Println(remaining, fns[remaining])
+	// fmt.Println(remaining, "(", r, ")", fns[remaining], st)
+	// return r + st
+	return 1
 }
 
 func part1(s string) int {
---
+++ 2022/day22/go.mod
@@ -0,0 +1,3 @@
+module example/aoc/2022/day22
+
+go 1.19

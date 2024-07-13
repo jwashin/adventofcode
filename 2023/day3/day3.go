3c930d5d0f84e8065aef7762e96c3ac7c1c428bf
Jim Washington <washington.jim@gmail.com>
2024-07-13 15:54:33 -0400

    day3 done

--- 2023/day3/day3.go
+++ 2023/day3/day3.go
@@ -13,6 +13,7 @@ var digits = "0123456789"
 func main() {
 	input, _ := os.ReadFile("input.txt")
 	fmt.Println("part 1:", part1(string(input)))
+	fmt.Println("part 2:", part2(string(input)))
 }
 
 func part1(s string) int {
@@ -24,6 +25,59 @@ func part1(s string) int {
 		total += v
 	}
 	return total
+}
+
+func part2(s string) int {
+	// 13270325 too low
+	s = strings.TrimSpace(s)
+	total := 0
+	xes := getAllStarCoordinates(s)
+	tableau := strings.Split(s, "\n")
+	for _, x := range xes {
+		j := getIntegersAdjacentToCoordinate(x, tableau)
+		if len(j) == 2 {
+			total += j[0] * j[1]
+		}
+	}
+	return total
+}
+
+func getIntegersAdjacentToCoordinate(c Coordinate, tableau []string) []int {
+	items := []int{}
+	testRows := []string{}
+	coords := map[IntId]int{}
+	if c.row > 0 {
+		testRows = append(testRows, tableau[c.row-1])
+	}
+	testRows = append(testRows, tableau[c.row])
+	if c.row < len(tableau)-1 {
+		testRows = append(testRows, tableau[c.row+1])
+	}
+	for _, row := range testRows {
+		if c.column > 0 {
+			i, err := getIntegerAtIndex(row, c.column-1)
+			if err == nil {
+				coords[i] = 1
+			}
+		}
+		i, err := getIntegerAtIndex(row, c.column)
+		if err == nil {
+			coords[i] = 1
+		}
+		if c.column < len(row)-1 {
+			i, err := getIntegerAtIndex(row, c.column+1)
+			if err == nil {
+				coords[i] = 1
+			}
+		}
+	}
+	if len(coords) == 2 {
+		for k := range coords {
+			items = append(items, k.value)
+		}
+	}
+
+	return items
 }
 
 func getIntegers(s string) ([]string, []int) {
@@ -51,22 +105,36 @@ func getIntegers(s string) ([]string, []int) {
 	return numbers, endIndexes
 }
 
-func getAllStarCoordinates(s string) [][]int {
+type Coordinate struct {
+	row    int
+	column int
+}
+
+func getAllStarCoordinates(s string) []Coordinate {
 	tableau := strings.Split(s, "\n")
-	stars := [][]int{}
+	stars := []Coordinate{}
 	for line := range tableau {
 		for column, char := range tableau[line] {
 			if char == '*' {
-				coordinate := []int{line, column}
-				stars = append(stars, coordinate)
+				stars = append(stars, Coordinate{line, column})
 			}
 		}
 	}
 	return stars
 }
 
-func getIntegerAtIndex(s string, index int) int {
+type IntId struct {
+	value int
+	start int
+	end   int
+}
+
+func getIntegerAtIndex(s string, index int) (IntId, error) {
 	candidate := string(s[index])
+	_, err := strconv.Atoi(candidate)
+	if err != nil {
+		return IntId{}, err
+	}
 	leftDone := false
 	leftidx := index
 	for !leftDone && leftidx > 0 {
@@ -74,22 +142,24 @@ func getIntegerAtIndex(s string, index int) int {
 		if strings.Contains(digits, string(s[leftidx])) {
 			candidate = string(s[leftidx]) + candidate
 		} else {
-			leftDone = false
+			leftDone = true
 		}
 	}
 	rightDone := false
 	rightidx := index
-	for !rightDone && rightidx <= len(s)-1 {
+	for !rightDone && rightidx <= len(s)-2 {
 		rightidx += 1
 		if strings.Contains(digits, string(s[rightidx])) {
 			candidate = candidate + string(s[rightidx])
 		} else {
-			rightDone = false
+			rightDone = true
 		}
 	}
-	t, _ := strconv.Atoi(candidate)
-	return t
-
+	t1, err1 := strconv.Atoi(candidate)
+	if err1 != nil {
+		return IntId{}, err
+	}
+	return IntId{t1, leftidx, rightidx}, nil
 }
 
 func getValidNumbers(s string) []int {
